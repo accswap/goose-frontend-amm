206d528178b66ca09ad4f7cdf106ff6694794dae
Cryptocode <crytocode99@gmail.com>
2021-05-04 00:47:32 +0700

    initial commit

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,5 +1,5 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xd538142dca6d06c8807a008513fe08cc774a60da',
+  cakeAddress: '0x80c1d7d8458bf83f6c37bcc4cd8f28d1eb085ddb',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
   lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
 }
