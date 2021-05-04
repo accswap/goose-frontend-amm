d549886d063d77ec3168afe60f44bcf50c079fa5
Cryptocode <crytocode99@gmail.com>
2021-05-04 11:14:12 +0700

    Update index.tsx

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react'
 import { Menu as UikitMenu, ConnectorId } from '@pancakeswap-libs/uikit'
 import { useWeb3React } from '@web3-react/core'
+import { allLanguages } from 'constants/localisation/languageCodes'
+import { LanguageContext } from 'hooks/LanguageContext'
 import useTheme from 'hooks/useTheme'
 import useGetPriceData from 'hooks/useGetPriceData'
 import { injected, bsc, walletconnect } from 'connectors'
@@ -8,6 +10,7 @@ import links from './config'
 
 const Menu: React.FC = props => {
   const { account, activate, deactivate } = useWeb3React()
+  const { selectedLanguage, setSelectedLanguage } = useContext(LanguageContext)
   const { isDark, toggleTheme } = useTheme()
   const cakePriceUsd = useGetPriceData()
 
@@ -30,6 +33,9 @@ const Menu: React.FC = props => {
       logout={deactivate}
       isDark={isDark}
       toggleTheme={toggleTheme}
+      currentLang={selectedLanguage?.code || ''}
+      langs={allLanguages}
+      setLang={setSelectedLanguage}
       cakePriceUsd={cakePriceUsd}
       {...props}
     />
